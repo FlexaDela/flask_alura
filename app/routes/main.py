06e3292e18a2e969c320e0989aaa1905866d1659
FlexaDela <mateusflexa2000@gmail.com>
2025-12-11 10:56:22 -0300

    fim da aula de criando login

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app.models.user import LoginPayLoad
+from pydantic import ValidationError
 
 main_bp = Blueprint("main_bp", __name__)
 
 @main_bp.route('/login/', methods=['POST'])
 def login():
-    return jsonify({"message":"Tela de Login"})
+    try:
+        raw_data = request.get_json()
+        user_data = LoginPayLoad(**raw_data)
+    except ValidationError as e:
+        return jsonify({"error":e.json()}), 400
+    except Exception as e:
+        return jsonify({"error":"erro durando a requisição"}), 500
+
+    if user_data.username == 'admin' and user_data.password == '123':
+        return jsonify({"message":f"Login realizado pelo {user_data.model_dump()}'"}), 200
+    else:
+        return jsonify({"message":f"Erro nas credenciais"}), 401
 
 @main_bp.route("/product/", methods=['GET'])
 def get_products():
